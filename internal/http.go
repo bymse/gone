201a2c60127c20c645de3453c38ec285fb9bd414
Egor Strelov <egorstrelov@gmail.com>
2026-09-10 09:25:01 +0300

    wip: parse headers

--- internal/http.go
+++ internal/http.go
@@ -15,18 +15,69 @@ const (
 	UnsupportedMethod
 	UnsupportedHttpVersion
 	InvalidRequestLine
+	MissingRequestLine
+	InvalidHeader
 )
+
+type httpRequest struct {
+	httpVersion   string
+	method        string
+	requestTarget string
+	headers       map[string][]string
+}
+
+func parseRequestLine(request *httpRequest, line string) httpRequestState {
+	if line == "" {
+		return MissingRequestLine
+	}
+
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return InvalidRequestLine
+	}
+
+	method := parts[0]
+	requestTarget := parts[1]
+	httpVersion := parts[2]
+
+	if httpVersion != "HTTP/1.1" {
+		return UnsupportedHttpVersion
+	}
+
+	if method != "GET" {
+		return UnsupportedMethod
+	}
+
+	request.httpVersion = httpVersion
+	request.method = method
+	request.requestTarget = requestTarget
+
+	return SectionHeaders
+}
+
+func parseHeader(headers map[string][]string, line string) httpRequestState {
+	key, value, found := strings.Cut(line, ":")
+	if !found {
+		return InvalidHeader
+	}
+
+	if key == "" || value == "" {
+		return InvalidHeader
+	}
+
+	headers[key] = append(headers[key], value)
+	return SectionHeaders
+}
 
 func ProcessRequest(reader io.Reader, writer io.Writer) error {
 	buff := make([]byte, 4096)
 	n, err := reader.Read(buff)
-	var method string
-	var requestTarget string
-	var httpVersion string
-	headers := make(map[string]string)
+	httpRequest := httpRequest{}
+	httpRequest.headers = make(map[string][]string)
 
 	sectionStart := 0
 	state := SectionRequestLine
+	sectionValueSet := false
 
 	if n > 0 {
 		for i := 0; i < n; i++ {
@@ -36,45 +87,33 @@ func ProcessRequest(reader io.Reader, writer io.Writer) error {
 				next = int16(buff[i+1])
 			}
 			var sectionValue string
-			if state != SectionBody && curr == '\r' && next == '\n' {
+			needsSectionValue := state == SectionRequestLine || state == SectionHeaders
+			if needsSectionValue && curr == '\r' && next == '\n' {
 				i++
 				next = -1
 				sectionValue = string(buff[sectionStart:i])
+				sectionValueSet = true
 			}
 
 			switch state {
 			case SectionRequestLine:
-				if sectionValue == "" {
+				if !sectionValueSet {
 					continue
 				}
-				parts := strings.Split(sectionValue, " ")
-				if len(parts) != 3 {
-					state = InvalidRequestLine
-					continue
-				}
-
-				method = parts[0]
-				requestTarget = parts[1]
-				httpVersion = parts[2]
-
-				if httpVersion != "HTTP/1.1" {
-					state = UnsupportedHttpVersion
-					continue
-				}
-
-				if method != "GET" {
-					state = UnsupportedMethod
-					continue
-				}
-
-				state = SectionHeaders
+				state = parseRequestLine(&httpRequest, sectionValue)
 			case SectionHeaders:
+				if !sectionValueSet {
+					continue
+				}
+
 				if sectionValue == "" {
 					state = SectionBody
 					continue
 				}
+
+				state = parseHeader(httpRequest.headers, sectionValue)
 			}
-			sectionValue = ""
+			sectionValueSet = false
 		}
 	}
 
